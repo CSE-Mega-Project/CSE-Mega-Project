1eafad6decc48aacbabca1c646219fbaa3e9b71a
Bharat Kumar <kumarbharat661@gmail.com>
2021-07-02 16:33:46 +0530

    View more page added

---
+++ Team Charlie/Landing Page/landing_page_viewmore/pagination.js
@@ -0,0 +1,29 @@
+var previousPage = "#page1";
+function clicked(button){
+    let newPage = "#page"+button.textContent;
+    document.querySelector(previousPage).classList.add("d-none");
+    document.querySelector(newPage).classList.remove("d-none");
+    previousPage = "#page"+button.textContent;
+}
+
+document.querySelector(".previous").addEventListener("click",function(){
+    let num = parseInt( previousPage[5])-1;
+    if(num>=1){
+        let newPage = "#page"+ num.toString();
+        console.log(newPage);
+        document.querySelector(previousPage).classList.add("d-none");
+        document.querySelector(newPage).classList.remove("d-none");
+        previousPage = newPage;
+    }
+});
+
+document.querySelector(".next").addEventListener("click",function(){
+    let num = parseInt( previousPage[5])+1;
+    if(num<=7){
+        let newPage = "#page"+ num.toString();
+        console.log(newPage);
+        document.querySelector(previousPage).classList.add("d-none");
+        document.querySelector(newPage).classList.remove("d-none");
+        previousPage = newPage;
+    }
+});
